0cc695a1b9b58dc7aea5eaff79797a227bcd227a
Kweku Adarkwa <1kweku@Kwekus-MacBook-Air-2.local>
2023-02-09 23:11:07 -0500

    error in thouht model

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,16 +5,25 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  removeReaction,
 } = require("../../controllers/thoughtController.js");
+const { route } = require("./userRoutes.js");
 
 // /api/courses
 router.route("/").get(getThoughts).post(createThought);
 
 // /api/courses/:courseId
 router
-  .route("/:courseId")
+  .route("/:thoughtId")
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+router.route("/:userId").post(createThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thought/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
